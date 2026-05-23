af2afab7a1265c3c5894ad7a8e7bfa8c85d16757
David LEGRAND <wawawaformation@gmail.com>
2026-05-23 17:46:10 +0200

    ajout des tools

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -20,11 +20,14 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
 from langchain.agents import create_agent
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.tools import tool
+
+
+from app.store import list_recipes, create_recipe, delete_recipe, RecipeCreate
 
 
 ### 1 - configuration du llm avec Azure AI ###
-load_dotenv()  # charge les variables d'environnement depuis le fichier .env
+load_dotenv() 
 
 llm = AzureAIChatCompletionsModel(
     endpoint=os.getenv("AZURE_AI_INFERENCE_ENDPOINT"),
@@ -32,13 +35,38 @@ llm = AzureAIChatCompletionsModel(
     credential=os.getenv("AZURE_AI_INFERENCE_API_KEY"),
 )
 
+
+
+### 2 - définition des outils pour l'agent (étape 2) ###
+
+@tool
+def list_recipes_tool() -> str:
+    """Retourne la liste des recettes actuelles."""
+    recipes = list_recipes()
+    if not recipes:
+        return "Aucune recette trouvée."
+    return "Des recettes ont été trouvées : " + ", ".join(f"{r.name}" for r in recipes)
+
+@tool
+def create_recipe_tool(name: str, ingredients: list[str]) -> str:
+    """Crée une nouvelle recette."""
+    recipe = create_recipe(data=RecipeCreate(name=name, ingredients=ingredients))
+    return f"Recette '{recipe.name}' créée avec succès."
+
+
+@tool
+def delete_recipe_tool(recipe_id: int) -> str:
+    """Supprime une recette par ID."""
+    success = delete_recipe(recipe_id)
+    if success:
+        return f"Recette avec ID {recipe_id} supprimée avec succès."
+    else:
+        return f"Aucune recette trouvée avec ID {recipe_id}."
+
 ### 2 - configuration de l'agent sans outils pour l'instant (étape 1) ###
-system_prompt = "Tu es un assistant de cuisine virtuel. Tu aides les utilisateurs à gérer leurs recettes de cuisine. Mais tu peux aussi parler de tout au besoin . Sois amical et serviable."
-agent = create_agent(llm, tools=[], system_prompt=system_prompt)
+system_prompt = "Tu es un assistant de cuisine virtuel. Tu aides les utilisateurs à gérer leurs recettes de cuisine et à créer de nouvelles recettes en bases de données. Mais tu peux aussi parler de tout au besoin . Sois amical et serviable."
+agent = create_agent(llm, tools=[list_recipes_tool, create_recipe_tool, delete_recipe_tool], system_prompt=system_prompt)
 router = APIRouter(prefix="/chat", tags=["chat"])
-
-
-
 
 class ChatRequest(BaseModel):
     message: str
@@ -56,7 +84,7 @@ def chat(request: ChatRequest) -> ChatResponse:
         
     })
    
-    #print("result from agent:", result)  # debug print pour voir la structure de la réponse
+
     return ChatResponse(
         ### 4 - retourner la réponse du modèle (étape 1) ###
         
